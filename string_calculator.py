b86718f4077e2ead9a859cbad0664cf611e184b1
saurabhmaun-techment <52454737+saurabhmaun-techment@users.noreply.github.com>
2022-03-31 12:31:12 +0530

    updates

--- string_calculator.py
+++ string_calculator.py
@@ -2,15 +2,40 @@
 
 import re
 
+
+def check_negetive_numbers(str1):
+    # filter negetive numbers and raise exception
+    negative_numbers = list(filter(lambda x: x < 0, str1))
+    if negative_numbers: raise Exception('negatives not allowed ' + str(negative_numbers))
+
 def add(numbers):
     if numbers == '': return 0
 
     numbers = map(int, re.findall(r"-?\d+", numbers))
     numbers = list(filter(lambda x: x < 1000, numbers))
 
-    # filter negetive numbers and raise exception
-    negative_numbers = list(filter(lambda x: x < 0, numbers))
-    if negative_numbers: raise Exception('negatives not allowed ' + str(negative_numbers))
+    check_negetive_numbers(numbers)
 
     # return the sum
     return sum(numbers)
+
+
+# sum of odd index
+# sum of even index
+# output : even - odd
+def new_add(numbers):
+    if numbers == '': return 0
+    numbers = map(int, re.findall(r"-?\d+", numbers))
+    numbers = list(filter(lambda x: x < 1000, numbers))
+    
+    check_negetive_numbers(numbers)
+
+    odd_index_sum = 0
+    even_index_sum = 0
+    for idx, x in enumerate(numbers):
+        if idx % 2 == 0:
+            even_index_sum = even_index_sum + x
+        else:
+            odd_index_sum = odd_index_sum + x
+
+    return (even_index_sum - odd_index_sum)
--- string_calculator_test.py
+++ string_calculator_test.py
@@ -34,9 +34,46 @@ def test_negative_numbers():
     with pytest.raises(Exception, match = r'negatives not allowed \[-1, -2, -3\]'):
         string_calculator.add('-1, -2, -3, 1, 2, 3')
 
-test_empty_string()
-test_add_two_numbers()
-test_add_multiple_numbers()
-test_add_new_line_character()
-test_add_diff_delimeters()
-test_negative_numbers()
+# Code Extension
+def test_empty_string():
+	# it should return 0 if the string is empty
+    print("Testing empty string")
+    assert string_calculator.new_add('') == 0
+
+def test_no_negetive():
+    with pytest.raises(Exception, match = r'negatives not allowed \[-1, -2, -3\]'):
+        string_calculator.new_add('-1, -2, -3, 1, 2, 3')
+
+def test_new_sum():
+    #test the sum with index
+    # 0 index will be countest as even
+    # Sum even index = 1 + 3
+    # Sum odd index = 2 + 4
+    # Returns = even - odd : 4 - 6 == -2 
+    assert string_calculator.new_add("1,2,3,4") == -2
+
+def test_add_two_numbers1():
+    # it should test 2 numbers
+    print("Testing 2 numbers in string")
+    assert string_calculator.new_add('1, 2') == -1
+
+def test_add_multiple_numbers1():
+	# it should test multiple numbers
+    print("Testing multiple numbers in string")
+    assert string_calculator.new_add('1, 2, 3') == 2
+
+def test_add_new_line_character1():
+	# it should test new line character in number string
+    print("Testing new line character in string")
+    assert string_calculator.new_add('1\n2,3') == 2
+
+def test_add_diff_delimeters1():
+	# it should test different delimeters in string
+    print("Testing different delimeters in string")
+    assert string_calculator.new_add("//;\n1;2") == -1
+
+def test_sample():
+    # 2,2,2,3
+    # 4 - 5 = -1
+    # 6 - 3 = 3
+    assert string_calculator.new_add("2,2,2,3") == -1
